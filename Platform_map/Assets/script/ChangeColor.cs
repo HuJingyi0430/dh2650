966261fb114e1c0431163753b6c5fc4b105e0a5c
HuJingyi0430 <violethjy@hotmail.com>
2023-04-04 16:18:53 +0200

    add gray
    left:puzzle1, up:puzzle2, right:puzzle3, down:area1

--- Platform_map/Assets/script/ChangeColor.cs
+++ Platform_map/Assets/script/ChangeColor.cs
@@ -6,14 +6,11 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    public int index = 0;
-
-
 
     private void Start()
     {
-        print("down arrow key is held down");
-        //FindObjectOfType<FadeToGray>().DoTheFade(index);
+        print("Set everything under Environment be gray");
+
         UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
         List<GameObject> rootObjects = new List<GameObject>(scene.rootCount + 1);
         scene.GetRootGameObjects(rootObjects);
@@ -29,9 +26,13 @@ public class ChangeColor : MonoBehaviour
                     foreach (Transform child in areas.transform)
                     {
                         print("The names of current root " + child.name);
-                        Renderer m_ObjectRenderer;
-                        m_ObjectRenderer = child.GetComponent<Renderer>();
-                        FindObjectOfType<FadeToGrayPuzzle1>().DoTheFadeP1(m_ObjectRenderer);
+                        Renderer[] m_ObjectRenderers;
+                        m_ObjectRenderers = child.GetComponents<Renderer>();
+                        foreach (Renderer m_ObjectRenderer in m_ObjectRenderers)
+                        {
+                            //print("The names of current renderer " + m_ObjectRenderer.name);
+                            FindObjectOfType<FadeToGray>().DoTheFade(m_ObjectRenderer);
+                        }
                     }
 
                 }
@@ -51,9 +52,9 @@ public class ChangeColor : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey("up"))
+        if (Input.GetKey("left"))
         {
-            print("Up arrow key is held down");
+            print("Up arrow key is held down, change the puzzle1 to colorful");
             //acess scene root game objects
             UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
             List<GameObject> rootObjects = new List<GameObject>(scene.rootCount + 1);
@@ -75,7 +76,7 @@ public class ChangeColor : MonoBehaviour
                                 print("The names of current root " + child.name);
                                 Renderer m_ObjectRenderer;
                                 m_ObjectRenderer = child.GetComponent<Renderer>();
-                                FindObjectOfType<FadeToGrayPuzzle1>().DoTheColorP1(m_ObjectRenderer);
+                                FindObjectOfType<FadeToGray>().DoTheColor(m_ObjectRenderer);
                             }
                         }
                     }
@@ -83,12 +84,12 @@ public class ChangeColor : MonoBehaviour
                 }
 
             }//for (int i = 0; i < rootObjects.Count; ++i)
-        }
+        }//Input.GetKey("left")
 
-        if (Input.GetKey("down"))
+        if (Input.GetKey("up"))
         {
 
-            print("down arrow key is held down");
+            print("down arrow key is held down, change the puzzle2 to colorful");
             //FindObjectOfType<FadeToGray>().DoTheFade(index);
             UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
             List<GameObject> rootObjects = new List<GameObject>(scene.rootCount + 1);
@@ -100,16 +101,87 @@ public class ChangeColor : MonoBehaviour
                 GameObject gameObject = rootObjects[i];
                 if (gameObject.name == "Environment")
                 {
-                    foreach (Transform puzzle1 in gameObject.transform)
+                    foreach (Transform puzzle2 in gameObject.transform)
                     {
-                        if (puzzle1.name == "puzzle1area")
+                        if (puzzle2.name == "puzzle2area")
                         {
-                            foreach (Transform child in puzzle1.transform)
+                            foreach (Transform child in puzzle2.transform)
                             {
                                 print("The names of current root " + child.name);
                                 Renderer m_ObjectRenderer;
                                 m_ObjectRenderer = child.GetComponent<Renderer>();
-                                FindObjectOfType<FadeToGrayPuzzle1>().DoTheFadeP1(m_ObjectRenderer);
+                                FindObjectOfType<FadeToGray>().DoTheColor(m_ObjectRenderer);
+                            }
+                        }
+                    }
+
+                }
+
+            }//for (int i = 0; i < rootObjects.Count; ++i)
+        }//Input.GetKey("up")
+
+
+
+        if (Input.GetKey("right"))
+        {
+
+            print("right arrow key is held down, change the puzzle3 to colorful");
+            //FindObjectOfType<FadeToGray>().DoTheFade(index);
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            List<GameObject> rootObjects = new List<GameObject>(scene.rootCount + 1);
+            scene.GetRootGameObjects(rootObjects);
+
+            // iterate root objects and do something
+            for (int i = 0; i < rootObjects.Count; ++i)
+            {
+                GameObject gameObject = rootObjects[i];
+                if (gameObject.name == "Environment")
+                {
+                    foreach (Transform puzzle3 in gameObject.transform)
+                    {
+                        if (puzzle3.name == "puzzle3area")
+                        {
+                            foreach (Transform child in puzzle3.transform)
+                            {
+                                print("The names of current root " + child.name);
+                                Renderer m_ObjectRenderer;
+                                m_ObjectRenderer = child.GetComponent<Renderer>();
+                                FindObjectOfType<FadeToGray>().DoTheColor(m_ObjectRenderer);
+                            }
+                        }
+                    }
+
+                }
+
+            }//for (int i = 0; i < rootObjects.Count; ++i)
+        }//Input.GetKey("right")
+
+
+        if (Input.GetKey("down"))
+        {
+
+            print("down arrow key is held down, change the puzzle2 to colorful");
+            //FindObjectOfType<FadeToGray>().DoTheFade(index);
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            List<GameObject> rootObjects = new List<GameObject>(scene.rootCount + 1);
+            scene.GetRootGameObjects(rootObjects);
+
+            // iterate root objects and do something
+            for (int i = 0; i < rootObjects.Count; ++i)
+            {
+                GameObject gameObject = rootObjects[i];
+                if (gameObject.name == "Environment")
+                {
+                    foreach (Transform area1 in gameObject.transform)
+                    {
+                        if (area1.name == "area1")
+                        {
+                            foreach (Transform child in area1.transform)
+                            {
+                                print("The names of current root " + child.name);
+                                Renderer m_ObjectRenderer;
+                                m_ObjectRenderer = child.GetComponent<Renderer>();
+                                FindObjectOfType<FadeToGray>().DoTheColor(m_ObjectRenderer);
                             }
                         }
                     }
